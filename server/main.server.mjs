18338fc28614222ed9ed964b400853d36b1a6c59
CambriHobbit <gabin.mussio@laposte.net>
2024-06-16 17:41:58 +0200

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CZWDGJF2.mjs";import"./chunk-CN7GQUE3.mjs";import"./chunk-PZQYRQHZ.mjs";import"./chunk-JRTUK7BF.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
